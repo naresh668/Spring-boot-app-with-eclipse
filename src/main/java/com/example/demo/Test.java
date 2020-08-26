d659d2895fff5468a2f961d0d1575bc5a01c74bb
naresh <nareshreddylgm@gmail.com>
2020-08-26 10:57:30 +0530

    tryone

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+ int x=20;
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		int y=30;
+		int z=30;fgdg
+		
+		
+		
+
+	}
+
+}
