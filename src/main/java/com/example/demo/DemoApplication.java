38dd37df675149111d956af6b5b42f59cc9c603f
naresh <nareshreddylgm@gmail.com>
2020-08-26 22:00:33 +0530

    sfsfwg

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,12 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
- int x=20;
+ 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		int y=30;
-		
-		
 		
 		
 
