2809daf72aa527c70188d3c41e224b8115309e3b
naresh <nareshreddylgm@gmail.com>
2020-08-26 23:06:49 +0530

    addedtestclass

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+ 
+	public static void main(String[] args) {
+		
+
+		in t=100;
+
+		in t=100;
+
+		
+
+	}
+
+}
